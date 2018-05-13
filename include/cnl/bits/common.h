e83ac9ade9c5dd3b8131550be6c9760d43024119
John McFarlane <github@john.mcfarlane.name>
2018-05-13 15:45:11 -0700

    Every non-trivial C++ project has a bunch of assert macros like this

--- include/cnl/bits/common.h
+++ include/cnl/bits/common.h
@@ -14,6 +14,59 @@
 #define CNL_STR_HELPER(x) #x
 #define CNL_STR(x) CNL_STR_HELPER(x)
 
+////////////////////////////////////////////////////////////////////////////////
+// CNL_ASSERT - with passing resemblance to code from github.com/Microsoft/GSL
+
+#if defined(CNL_LIKELY)
+#error CNL_LIKELY already defined.
+#elif defined(CNL_ASSUME)
+#error CNL_ASSUME already defined.
+#elif defined(CNL_ASSERT)
+#error CNL_ASSERT already defined.
+#endif
+
+// Customization point: define either CNL_DEBUG or CNL_RELEASE
+// to affect how contract violations are handled
+// in builds that don't support contract [[attributes]].
+#if defined(CNL_DEBUG) && defined(CNL_RELEASE)
+#error Mutually exclusive macros, CNL_DEBUG are both defined CNL_RELEASE.
+#elif !defined(CNL_DEBUG) && !defined(CNL_RELEASE)
+#if defined(NDEBUG)
+#define CNL_RELEASE
+#else
+#define CNL_DEBUG
+#endif
+#endif
+
+// CNL_LIKELY - hints that a condition is likely to be true
+#if defined(__clang__) || defined(__GNUC__)
+#define CNL_LIKELY(CONDITION) __builtin_expect(!!(CONDITION), 1)
+#define CNL_UNLIKELY(CONDITION) __builtin_expect(!!(CONDITION), 0)
+#else
+#define CNL_LIKELY(CONDITION) (!!(CONDITION))
+#define CNL_UNLIKELY(CONDITION) (!!(CONDITION))
+#endif
+
+// CNL_ASSUME - hints that a condition *must* be true
+#ifdef _MSC_VER
+#define CNL_ASSUME(cond) __assume(cond)
+#elif defined(__GNUC__)
+#define CNL_ASSUME(cond) ((cond) ? static_cast<void>(0) : __builtin_unreachable())
+#else
+#define CNL_ASSUME(cond) static_cast<void>((cond) ? 0 : 0)
+#endif
+
+// CNL_ASSERT - enforces that a condition *must* be true
+#if defined(CNL_RELEASE)
+// in release builds,
+#define CNL_ASSERT(CONDITION) CNL_ASSUME(CONDITION)
+#elif defined(CNL_DEBUG)
+#include <exception>
+#define CNL_ASSERT(CONDITION) (CNL_LIKELY(CONDITION) ? static_cast<void>(0) : std::terminate())
+#else
+#error internal library error
+#endif
+
 namespace cnl {
     namespace _impl {
         ////////////////////////////////////////////////////////////////////////////////
